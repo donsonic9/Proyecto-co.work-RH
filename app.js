c49b7dc660b4489da6cd418491197487f1d09f9d
donsonic9 <117784209+donsonic9@users.noreply.github.com>
2023-05-24 21:13:27 -0300

    test-new-page

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 // Observer for scroll animation on y-axis
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
-    console.log(entry)
+    // console.log(entry)
 
     if(entry.isIntersecting) {
       entry.target.classList.add('show');
@@ -13,12 +13,11 @@ const observer = new IntersectionObserver((entries) => {
   })
 })
 
-
 const hiddennElements = document.querySelectorAll('.hiddenn');
 hiddennElements.forEach((el) => observer.observe(el));
 
-// Collapse navbar on click
 
+// Collapse navbar on click
 function collapseNavbar() {
   var navbarCollapse = document.getElementById('navbarNavAltMarkup');
   var bsCollapse = new bootstrap.Collapse(navbarCollapse, { toggle: false });
